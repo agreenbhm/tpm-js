e8bd8c0d5d96592680545c1e2020bdbcb9037d1f
Drew Green <agreenbhm@gmail.com>
2022-07-28 11:53:58 -0400

    Update dinit.sh

--- dinit.sh
+++ dinit.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+if [ $(id -g "${USER}") -lt 1000 ]; then
+  gid=$(for group in $(id -G); do echo $group; done | sort --human-numeric-sort | tail -1)
+else
+  gid=$(id -g "${USER}")
+fi
 if [ ! -d "build-web" ]; then
-  chown :"$(id -g "${USER}")" .
+  chown :"$gid" .
   chmod 775 .
   chmod g+s .
   mkdir build-web
-  chown :"$(id -g "${USER}")" build-web
+  chown :"$gid" build-web
   chmod 775 build-web
   chmod g+s build-web
 fi
@@ -13,8 +18,9 @@ if [ -d ".git" ]; then
     git submodule update --init --recursive
   fi
 fi
+
 docker build \
   --build-arg user="$(id -u "${USER}")" \
-  --build-arg group="$(id -g "${USER}")" \
+  --build-arg group="$gid" \
   -t tpm-js-builder-image .
 
